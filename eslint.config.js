8c850179af4add85f7e1e20af808660b3531f719
Subin Min <tnqls5417@naver.com>
2026-01-11 18:21:46 +0900

    feat: add format and lint tool

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+const unusedImportsPlugin = require("eslint-plugin-unused-imports");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+      "unused-imports": unusedImportsPlugin,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": "warn",
+      "no-console": "warn",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
   },
 ]);
